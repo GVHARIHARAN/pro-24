39e465a5ddf675fad6fdfa3b423b081cb2ec0cfc
hxrihxrxn <skhariharan556@gmail.com>
2025-03-24 12:25:01 +0530

    pro

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,31 @@ import One from '../src/pro'
 // import Sum from '../src/componments/navber'
 import Sample from '../src/pro1'
 // import Order from'../src/pro'
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
     <>
-    
-      
+
+
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<One />} />
-           <Route path='/pro1' element={<Sample />} />
+          <Route path='/pro1' element={<Sample />} />
           {/* <Route path='/pro/' element={<Order />} />  */}
-          </Routes>
-          </BrowserRouter>
-        </>
-        )
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
--- src/pro.jsx
+++ src/pro.jsx
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
+
 // import { Footer } from "flowbite-react";
 
 
